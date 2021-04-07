66f31a49db3f1877948c17c0c9a39b1afbdc4969
Mac Rowe <m.rowe42@gmail.com>
2021-04-07 16:30:29 -0400

    commit tree f7e8855249546b3dfbf1e996825135e661072554

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  post '/articles' do 
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+  
+  get '/articles/new' do 
+    @article = Article.new
+    erb :new
+  end 
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  delete '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.delete
+  end 
+
+
+
+
 end
